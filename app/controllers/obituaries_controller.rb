93dc29f4d085f74567c5eab6eaebbf3507e8b3e7
Adam Hutchison <liveh2o@gmail.com>
2011-04-05 20:59:26 -0500

    Can now search by first/last name

--- app/controllers/obituaries_controller.rb
+++ app/controllers/obituaries_controller.rb
@@ -1,35 +1,13 @@
 class ObituariesController < ApplicationController
   # GET /obituaries
-  # GET /obituaries.xml
   def index
-    @obituaries = Obituary.page(params[:page])
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @obituaries }
-    end
-  end
-
-  # GET /obituaries/1
-  # GET /obituaries/1.xml
-  def show
-    @obituary = Obituary.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @obituary }
-    end
+    @search = Obituary.search(params[:search])
+    @obituaries = @search.page(params[:page])
   end
 
   # GET /obituaries/new
-  # GET /obituaries/new.xml
   def new
     @obituary = Obituary.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @obituary }
-    end
   end
 
   # GET /obituaries/1/edit
@@ -38,46 +16,32 @@ class ObituariesController < ApplicationController
   end
 
   # POST /obituaries
-  # POST /obituaries.xml
   def create
     @obituary = Obituary.new(params[:obituary])
 
-    respond_to do |format|
-      if @obituary.save
-        format.html { redirect_to(@obituary, :notice => 'Obituary was successfully created.') }
-        format.xml  { render :xml => @obituary, :status => :created, :location => @obituary }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @obituary.errors, :status => :unprocessable_entity }
-      end
+    if @obituary.save
+      redirect_to(@obituary, :notice => 'Obituary was successfully created.')
+    else
+      render :action => "new"
     end
   end
 
   # PUT /obituaries/1
-  # PUT /obituaries/1.xml
   def update
     @obituary = Obituary.find(params[:id])
 
-    respond_to do |format|
-      if @obituary.update_attributes(params[:obituary])
-        format.html { redirect_to(@obituary, :notice => 'Obituary was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @obituary.errors, :status => :unprocessable_entity }
-      end
+    if @obituary.update_attributes(params[:obituary])
+      redirect_to(@obituary, :notice => 'Obituary was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
   # DELETE /obituaries/1
-  # DELETE /obituaries/1.xml
   def destroy
     @obituary = Obituary.find(params[:id])
     @obituary.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(obituaries_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(obituaries_url)
   end
 end
